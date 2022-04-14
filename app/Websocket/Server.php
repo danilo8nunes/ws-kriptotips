c221e87266cf16b7dc761c3b0b70d2d8f6be3991
Danilo Nunes <danilo8nunes@gmail.com>
2022-04-13 23:35:03 -0300

    refatorações

--- app/Websocket/Server.php
+++ app/Websocket/Server.php
@@ -5,6 +5,6 @@ require_once './vendor/autoload.php';
 use Ratchet\App;
 use App\Websocket\Operation;
 
-$app = new App('www.darkcrypti.com.br', 8089);
+$app = new App('www.darkcrypt.com.br', 8089);
 $app->route('/operation', new Operation(), ['*']);
 $app->run();
