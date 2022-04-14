b657c19dc984522c49e1c4d10bd7f81aea60640a
Danilo Nunes <danilo8nunes@gmail.com>
2022-04-13 23:11:57 -0300

    refatorações

--- app/Websocket/Server.php
+++ app/Websocket/Server.php
@@ -4,8 +4,6 @@ require_once './vendor/autoload.php';
 
 use Ratchet\App;
 use App\Websocket\Operation;
-use Ratchet\Server\EchoServer;
-
 
 $app = new App('localhost', 8080);
 $app->route('/operation', new Operation(), ['*']);
