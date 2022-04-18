1c4548e6fa0c45bf7b01c2e73323fee8f64e0688
Danilo Nunes <danilo8nunes@gmail.com>
2022-04-18 10:08:37 -0300

    refatoração

--- public/index.php
+++ public/index.php
@@ -1,1 +1,33 @@
-TESTE
+<?php
+
+require_once './vendor/autoload.php';
+
+$data = $_POST;
+
+if (!empty($data['app_token']) && $data['app_token'] === APP_TOKEN) {
+
+  $urlBase = ENVIRONMENT === 'prod' ? CONF_URL_BASE : CONF_URL_TEST;
+
+  unset($data['app_token']);
+
+  \Ratchet\Client\connect('ws://' . $urlBase . ':8080/operation')->then(function ($conn) use ($data) {
+    $conn->send(json_encode($data));
+
+    $conn->close();
+  }, function ($e) {
+    echo "Could not connect: {$e->getMessage()}\n";
+  });
+
+  if (!empty($data['operation'])) {
+    echo json_encode([
+      'success' => 'operation sent',
+      'operation' => $data['operation']
+    ]);
+  }
+} elseif (empty($data)) {
+  return '../view.html';
+} else {
+  echo json_encode([
+    'error' => 'invalid token',
+  ]);
+}
