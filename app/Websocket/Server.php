3439491261a6cd80fd23ef8deebc07a03f4cf590
Danilo Nunes <danilo8nunes@gmail.com>
2022-04-18 10:52:32 -0300

    refatoração

--- app/Websocket/Server.php
+++ app/Websocket/Server.php
@@ -7,7 +7,7 @@ use App\Websocket\Operation;
 
 $urlBase = ENVIRONMENT === 'prod' ? CONF_URL_BASE : CONF_URL_TEST;
 
-$app = new App($urlBase, 443, '54.243.129.215');
+$app = new App($urlBase, 443, '0.0.0.0');
 
 $app->route('/operation', new Operation(), ['*']);
 
