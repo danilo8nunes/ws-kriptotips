68f4ccc6b6f43cc5717f6cc18bb8d56e319df77b
Danilo Nunes <danilo8nunes@gmail.com>
2022-04-18 11:53:03 -0300

    refatoração

--- app/Websocket/Server.php
+++ app/Websocket/Server.php
@@ -7,8 +7,9 @@ use App\Websocket\Operation;
 
 $url = ENVIRONMENT === 'prod' ? CONF_URL_BASE : CONF_URL_TEST;
 $port =  ENVIRONMENT === 'prod' ? CONF_PORT_BASE : CONF_PORT_TEST;
+$ip =  ENVIRONMENT === 'prod' ? CONF_IP_BASE : CONF_IP_TEST;
 
-$app = new App($url, $port);
+$app = new App($url, $port, $ip);
 
 $app->route('/operation', new Operation(), ['*']);
 
--- app/support/config.php
+++ app/support/config.php
@@ -12,6 +12,12 @@ define("CONF_URL_TEST", "localhost");
 /**
  * PROJECT URLs
  */
+define("CONF_IP_BASE", "54.243.129.215");
+define("CONF_IP_TEST", "127.0.0.1");
+
+/**
+ * PROJECT PORTs
+ */
 define("CONF_PORT_BASE", 80);
 define("CONF_PORT_TEST", 8080);
 
