c502da8841022250453fc019dedce407108bc844
Danilo Nunes <danilo8nunes@gmail.com>
2022-04-19 09:31:49 -0300

    refatorações

--- app/Websocket/Server.php
+++ app/Websocket/Server.php
@@ -6,10 +6,10 @@ use Ratchet\App;
 use App\Websocket\Operation;
 
 $url = ENVIRONMENT === 'prod' ? CONF_URL_BASE : CONF_URL_TEST;
-$port =  ENVIRONMENT === 'prod' ? CONF_PORT_BASE : CONF_PORT_TEST;
+$port =  ENVIRONMENT === 'prod' ? getenv('PORT') : CONF_PORT_TEST;
 $ip =  ENVIRONMENT === 'prod' ? CONF_IP_BASE : CONF_IP_TEST;
 
-$app = new App($url, $PORT, $ip);
+$app = new App($url, $port, $ip);
 
 $app->route('/operation', new Operation(), ['*']);
 
